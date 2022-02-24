478d90e3fe862cb1b53fde6d43c8268736558c49
dinbaba <magnusguldberg@live.dk>
2022-02-24 15:59:51 +0100

    BO, objective function and plots added

--- clfCLF.py
+++ clfCLF.py
@@ -2,8 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import GPyOpt
 from torchvision import datasets, transforms, utils
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import ParameterSampler, RandomizedSearchCV, cross_val_score
 from scipy.stats import uniform
 import random
@@ -69,22 +67,21 @@ for params in param_list:
     max_score_per_iteration.append(current_best_score)
     i += 1
     print(f'It took {end - start} seconds')
-"""
+
 ## define the domain of the considered parameters
-n_estimators = tuple(np.arange(1, 101, 1, dtype=np.int))
+n_estimators = tuple(np.arange(5, 95, 5, dtype=np.int))
 # print(n_estimators)
-max_depth = tuple(np.arange(10, 110, 10, dtype=np.int))
-# max_features = ('log2', 'sqrt', None)
-max_features = (0, 1)
-# criterion = ('gini', 'entropy')
-criterion = (0, 1)
+learning_rate=range(0.1, 1.5, 0.2)
+# algorithm = ("SAMME", "SAMME.R")
+algorithm = (0, 1)
+# random_state = (32, None)
+random_state = (0, 1)
 
 # define the dictionary for GPyOpt
 domain = [{'name': 'n_estimators', 'type': 'discrete', 'domain': n_estimators},
-          {'name': 'max_depth', 'type': 'discrete', 'domain': max_depth},
-          {'name': 'max_features', 'type': 'categorical', 'domain': max_features},
-          {'name': 'criterion', 'type': 'categorical', 'domain': criterion}]
-
+          {'name': 'learning_rate', 'type': 'discrete', 'domain': learning_rate},
+          {'name': 'algorithm', 'type': 'categorical', 'domain': algorithm},
+          {'name': 'random_state', 'type': 'categorical', 'domain': random_state}]
 
 ## we have to define the function we want to maximize --> validation accuracy,
 ## note it should take a 2D ndarray but it is ok that it assumes only one point
@@ -97,23 +94,81 @@ def objective_function(x):
     print(param)
     # we have to handle the categorical variables
     if param[2] == 0:
-        max_f = 'log2'
+        algorithm = "SAMME"
     elif param[2] == 1:
-        max_f = 'sqrt'
-    else:
-        max_f = None
+        algorithm = "SAMME.R"
 
     if param[3] == 0:
-        crit = 'gini'
+        random_state = 32
     else:
-        crit = 'entropy'
+        random_state = None
 
     # create the model
-    model = RandomForestClassifier(n_estimators=int(param[0]), max_depth=int(param[1]), max_features=max_f,
-                                   criterion=crit, oob_score=True, n_jobs=-1)
+
+    model = AdaBoostClassifier(n_estimators=int(param[0]), learning_rate=float(param[1]),
+                               algorithm=algorithm, random_state=random_state)
 
     # fit the model
     model.fit(Xtrain, ytrain)
-    print(model.oob_score_)
-    return - model.oob_score_
-"""
+    print(model.score(Xtest,ytest))
+    return - model.score(Xtest,ytest)
+
+opt = GPyOpt.methods.BayesianOptimization(f = objective_function,   # function to optimize
+                                              domain = domain,         # box-constrains of the problem
+                                              acquisition_type = 'EI' ,      # Select acquisition function MPI, EI, LCB
+                                             )
+opt.acquisition.exploration_weight=0.5
+
+opt.run_optimization(max_iter = 15)
+
+x_best = opt.X[np.argmin(opt.Y)]
+print("The best parameters obtained: n_estimators=" + str(x_best[0]) + ", learning_rate=" + str(x_best[1]) + ", algorithm=" + str(
+    x_best[2])  + ", random_state=" + str(
+    x_best[3]))
+
+## comparison between random search and bayesian optimization
+## we can plot the maximum oob per iteration of the sequence
+
+# collect the maximum each iteration of BO, note that it is also provided by GPOpt in Y_Best
+y_bo = np.maximum.accumulate(-opt.Y).ravel()
+# define iteration number
+xs = np.arange(1,21,1)
+
+plt.plot(xs, max_score_per_iteration, 'o-', color = 'red', label='Random Search')
+plt.plot(xs, y_bo, 'o-', color = 'blue', label='Bayesian Optimization')
+plt.legend()
+plt.xlabel('Iterations')
+plt.ylabel('Out of bag error')
+plt.title('Comparison between Random Search and Bayesian Optimization')
+plt.show()
+
+#We start by taking a look a the model subclass of our Bayesian optimization object
+print(opt.model.input_dim)
+#so there is 6 dimensions, that is 1 for each of n_estimators and depth which are discrete variables.
+# the remaining 4 is because one-out-of-K encoding is using for the categorical variables max_features and criterion
+#we can also look at kernel parameters
+print(opt.model.get_model_parameters_names())
+#and get the current fitted values
+print(opt.model.get_model_parameters())
+#To get a plot of the acquisition function we use the function opt.acquisition.acquisition_function
+#first we define a sensible grid for the first to parameters
+#indexing='ij' ensures that x/y axes are not flipped (which is default):
+#we also add two extra axes for the categorical varibles and here fix these to 0 ('log2' and 'gini')
+#note that the acqusition function can actually take any value not only integers as it lives in the GP space (here 0.5 intervals)
+#and it is quite fast to evaluate - here in 40000 points
+n_feat = np.arange(1,101,0.5)
+max_d = np.arange(10,110,0.5)
+pgrid = np.array(np.meshgrid(n_feat, max_d,[1],[0],[1],[0],indexing='ij'))
+print(pgrid.reshape(6,-1).T.shape)
+#we then unfold the 4D array and simply pass it to the acqusition function
+acq_img = opt.acquisition.acquisition_function(pgrid.reshape(6,-1).T)
+#it is typical to scale this between 0 and 1:
+acq_img = (-acq_img - np.min(-acq_img))/(np.max(-acq_img - np.min(-acq_img)))
+#then fold it back into an image and plot
+acq_img = acq_img.reshape(pgrid[0].shape[:2])
+plt.figure()
+plt.imshow(acq_img.T, origin='lower',extent=[n_feat[0],n_feat[-1],max_d[0],max_d[-1]])
+plt.colorbar()
+plt.xlabel('n_features')
+plt.ylabel('max_depth')
+plt.title('Acquisition function')
